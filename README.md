e6eeef0a2a19a6aa0c86e63ecd06d94e7a6f95c2
ikhzz <fikriztm@gmail.com>
2020-09-26 19:33:29 +0800

    lightbox and blog section need refactor script

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 ##### ikhzz.github.io
+
+##### guide-language    : Youtube-Web Programming UNPAS
+##### guide-example     : Youtube-The WebShala
+
+##### image             : www.unsplash.com/random
+##### other             : www.google.com
--- js/script.js
+++ js/script.js
@@ -1,13 +1,10 @@
 const   filter = document.querySelector('.filter'),
         filters = filter.children,
         totalFilter = filters.length,
-        itemPort = document.querySelectorAll('p-item'),
+        itemPort = document.querySelectorAll('.p-item li'),
         totalPort = itemPort.length;
 
-const filtera = document.querySelectorAll('.filter button')
-
-console.log(filter)
-console.log(filtera)
+const filtera = document.querySelectorAll('.filter li')
 
 for(let i=0; i < totalFilter; i++) {
     filters[i].addEventListener('click', (e)=> {
@@ -16,6 +13,81 @@ for(let i=0; i < totalFilter; i++) {
 }
 filtera.forEach(element => {
     element.addEventListener('click', (e)=> {
-        console.log(element.innerHTML)
+        filter.querySelector('.active').classList.remove('active');
+        element.classList.add('active')
+        
+        const value = element.getAttribute('data-filter');
+        itemPort.forEach( i => {
+            if(value == i.getAttribute('data-category')) {
+                i.classList.remove('hide')
+                i.classList.add('show')
+            } else if(value == 'all') {
+                i.classList.remove('hide')
+                i.classList.add('show')
+            } else {
+                i.classList.remove('show')
+                i.classList.add('hide')
+            }
+        })
     })
 });
+
+// filter end
+
+const   lightboxport = document.querySelector('.lightport'),
+        lightportContent = document.querySelector('.lightportContent img'),
+        lightportCaptiontext = document.querySelector('.lightportCaption .text'),
+        lightportCaptioncounter = document.querySelector('.lightportCaption .counter'),
+        lightportClose = document.querySelector('.lightportClose')
+let itemindex = 0;
+
+// itemPort.forEach( () => {
+//     document.addEventListener('click', (e,index) => {
+//         console.log( e)
+//     })
+// })
+
+itemPort.forEach(tesfunc )
+function tesfunc(item, index) {
+    //console.log(item, index)
+    item.addEventListener('click', ()=> {
+        itemindex = index;
+        console.log(item);
+        changePort(item);
+        togglelightbox();
+    })
+}
+
+function changePort(data) {
+    imgsrc = itemPort[itemindex].querySelector('li img').getAttribute('src');
+    lightportContent.src = imgsrc;
+    lightportCaptioncounter.innerHTML = (itemindex+1) + ' of ' + itemPort.length;
+    //console.log(lightportContent)
+}
+function togglelightbox() {
+    lightboxport.classList.toggle('show')
+    
+}
+console.log(lightportClose)
+lightportClose.addEventListener('click', ()=> {
+    lightboxport.classList.toggle('show')
+})
+
+function nextport() {
+    //console.log(itemindex)
+    if(itemindex == itemPort.length -1) {
+        itemindex = 0
+    } else {
+        itemindex++;
+    }
+    changePort();
+}
+function prevport() {
+    //console.log(itemindex)
+    if(itemindex == 0) {
+        itemindex = itemPort.length -1
+    } else {
+        itemindex--;
+    }
+    changePort();
+}
