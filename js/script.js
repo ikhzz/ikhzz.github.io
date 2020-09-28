65861bb1c51c2001c226fe6e7cd073ec147c279d
ikhzz <fikriztm@gmail.com>
2020-09-28 14:20:40 +0800

    refactor and thinking something fun

--- js/script.js
+++ js/script.js
@@ -1,19 +1,12 @@
-const   filter = document.querySelector('.filter'),
-        filters = filter.children,
-        totalFilter = filters.length,
-        itemPort = document.querySelectorAll('.p-item li'),
-        totalPort = itemPort.length;
+const   itemPort = document.querySelectorAll('.p-item li'),
+        filter = document.querySelectorAll('.filter li'),
+        navlist = document.querySelectorAll('.nav li'),
+        section = document.querySelectorAll('section')
+let     itemindex = 0;
 
-const filtera = document.querySelectorAll('.filter li')
-
-for(let i=0; i < totalFilter; i++) {
-    filters[i].addEventListener('click', (e)=> {
-        //console.log(filters[i].innerHTML)
-    })
-}
-filtera.forEach(element => {
+filter.forEach(element => {
     element.addEventListener('click', (e)=> {
-        filter.querySelector('.active').classList.remove('active');
+        document.querySelector('.filter').querySelector('.active').classList.remove('active');
         element.classList.add('active')
         
         const value = element.getAttribute('data-filter');
@@ -34,56 +27,35 @@ filtera.forEach(element => {
 
 // filter end
 
-const   lightboxport = document.querySelector('.lightport'),
-        lightportContent = document.querySelector('.lightportContent img'),
-        lightportCaptiontext = document.querySelector('.lightportCaption .text'),
-        lightportCaptioncounter = document.querySelector('.lightportCaption .counter'),
-        lightportClose = document.querySelector('.lightportClose')
-let itemindex = 0;
-
-// itemPort.forEach( () => {
-//     document.addEventListener('click', (e,index) => {
-//         console.log( e)
-//     })
-// })
-
-itemPort.forEach(tesfunc )
-function tesfunc(item, index) {
-    //console.log(item, index)
+itemPort.forEach(((item, index) => {
     item.addEventListener('click', ()=> {
         itemindex = index;
-        changePort(item);
+        changePort();
         togglelightbox();
-    })
-}
+    }); 
+}) );
 
-function changePort(data) {
+function changePort() {
     imgsrc = itemPort[itemindex].querySelector('li img').getAttribute('src');
-    lightportContent.src = imgsrc;
-    lightportCaptioncounter.innerHTML = (itemindex+1) + ' of ' + itemPort.length;
-    //console.log(lightportContent)
+    category = itemPort[itemindex].getAttribute('data-category');
+    document.querySelector('.lightportContent img').src = imgsrc;
+    document.querySelector('.lightportCaption .counter').innerHTML = (itemindex+1) + ' of ' + itemPort.length;
+    document.querySelector('.lightportCaption .text').innerHTML = category[0].toUpperCase() + category.slice(1);
 }
 function togglelightbox() {
-    lightboxport.classList.toggle('show')
-    
+    document.querySelector('.lightport').classList.toggle('show');
 }
-lightportClose.addEventListener('click', ()=> {
-    lightboxport.classList.toggle('show')
-})
-
 function nextport() {
-    //console.log(itemindex)
     if(itemindex == itemPort.length -1) {
-        itemindex = 0
+        itemindex = 0;
     } else {
         itemindex++;
     }
     changePort();
 }
 function prevport() {
-    //console.log(itemindex)
     if(itemindex == 0) {
-        itemindex = itemPort.length -1
+        itemindex = itemPort.length -1;
     } else {
         itemindex--;
     }
@@ -92,43 +64,34 @@ function prevport() {
 
 // lighbox end
 
-const   nav = document.querySelector('.nav'),
-        navlist = document.querySelectorAll('.nav li'),
-        totalnav = navlist.length,
-        section = document.querySelectorAll('section')
-        console.log(section)
-
-for(let i=0; i<totalnav; i++) {
-    const a = navlist[i].querySelector('a');
+navlist.forEach(((items)=> {
+    const a = items.querySelector('li a')
     a.addEventListener('click', ()=> {
-        for(let k=0; k<section.length;k++) {
-            section[k].classList.remove('back')
-        }
-        for(let j=0; j<totalnav; j++) {
-            console.log()
-            if(navlist[j].querySelector('a').classList.contains('active')) {
-                section[j].classList.add('back')
+        section.forEach((items) => {
+            items.classList.remove('back');
+        })
+        navlist.forEach((items,index)=> {
+            if(items.querySelector('a').classList.contains('active')) {
+                section[index].classList.add('back');
             }
-            navlist[j].querySelector('a').classList.remove('active')
-        }
-        a.classList.add('active')
-        showSection(a);
-    })
-}
+            items.querySelector('a').classList.remove('active');
+        })
+        a.classList.add('active');
+        (()=> {
+            const item = items.querySelector('li a').getAttribute('href').split('#')[1];
+            section.forEach((item)=> {
+                item.classList.remove('active');
+            })
+            document.querySelector('#'+item).classList.add('active');
+        })();
+    });
+}));
 
-function showSection(element) {
-    const a = element.getAttribute('href').split('#')[1]
-    for(let i=0; i<section.length;i++) {
-        section[i].classList.remove('active')
-    }
-    document.querySelector('#'+a).classList.add('active')
-}
+// section and nav end
 
-const   nyam = document.querySelector('.nyam'),
-        content = document.querySelector('.content'),
-        aside = document.querySelector('aside')
-
-nyam.addEventListener('click', () => {
-    aside.classList.toggle('open')
-    content.classList.toggle('openc')
+document.querySelector('.nyam').addEventListener('click', () => {
+    document.querySelector('aside').classList.toggle('open')
+    document.querySelector('.content').classList.toggle('openc')
 })
+
+// burger end
